feaaeda0adfbf4cf7c2dfe1397b4e3699d921c07
Carlos Pizarro Morales <carlospizarromorales@gmail.com>
2021-05-22 15:37:20 -0400

    terminado

---
+++ README.md
@@ -0,0 +1,5 @@
+# Desafio Multiplicacion con Javascript
+
+Desafío de Javascript 20210520
+Se solicita uso de let y funciones arrow.
+Listo. 
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,19 @@
 //logica basica
-let userNumber = null;
-window.onload = () => {
-    console.log(multiplicar(16));
-    console.log(factorial(16));
-}
+let formulario = document.getElementById('formulario');
+let tablaFactorial = [];
+let tabla = [];
+
+formulario.addEventListener("submit", event => {
+    event.preventDefault();
+    const valor = document.getElementById('input').value;
+    multiplicar(valor);
+    factorial(valor);
+    dibuja();
+});
+
+//funciones
 const multiplicar = (x) => {
-    let tabla = [];
+
     for (let i = 1; i <= x; i++) {
         tabla.push(x * i);
     }
@@ -13,7 +21,7 @@ const multiplicar = (x) => {
 }
 
 const factorial = (x) => {
-    let tablaFactorial = [];
+
     let acum = null;
     for (let i = 1; i <= x; i++) {
         for (let j = 1; j <= i; j++) {
@@ -23,4 +31,22 @@ const factorial = (x) => {
         acum = 1;
     }
     return tablaFactorial;
+}
+
+function dibuja() {
+    let cajon1 = document.getElementById('resultadoUno');
+    let cajon2 = document.getElementById('resultadoDos');
+    cajon1.innerHTML = "";
+    cajon2.innerHTML = "";
+    console.log('todo ok por aca...');
+    cajon1.innerHTML += '<p class="mensaje">......processing</p>';
+    cajon2.innerHTML += '<p class="mensaje">......processing</p>';
+    for (let i = 0; i < tabla.length; i++) {
+        cajon1.innerHTML += `${tabla[0]} x ${i+1} = ${tabla[i]} <br>`;
+    }
+    for (let i = 0; i < tablaFactorial.length; i++) {
+        cajon2.innerHTML += `Factorial de ${i+1} = ${tablaFactorial[i]} <br>`;
+    }
+    tablaFactorial = [];
+    tabla = [];
 }
