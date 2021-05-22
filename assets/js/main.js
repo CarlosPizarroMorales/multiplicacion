ae2d230254523c2effdde7412e8a1740475f911d
Carlos Pizarro Morales <carlospizarromorales@gmail.com>
2021-05-22 10:16:09 -0400

    funciones listas

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,26 @@
 //logica basica
 let userNumber = null;
-
+window.onload = () => {
+    console.log(multiplicar(16));
+    console.log(factorial(16));
+}
 const multiplicar = (x) => {
     let tabla = [];
-    for (; i < x; i++)
+    for (let i = 1; i <= x; i++) {
+        tabla.push(x * i);
+    }
+    return tabla;
+}
+
+const factorial = (x) => {
+    let tablaFactorial = [];
+    let acum = null;
+    for (let i = 1; i <= x; i++) {
+        for (let j = 1; j <= i; j++) {
+            acum *= j
+        }
+        tablaFactorial.push(acum);
+        acum = 1;
+    }
+    return tablaFactorial;
 }
